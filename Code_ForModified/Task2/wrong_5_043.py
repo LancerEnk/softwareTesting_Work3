c5b53ed8aef885940d2c9ef54d1f98729df926e9
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-27 23:51:45 +0800

    Update wrong_5_043.py

--- Code_ForModified/Task2/wrong_5_043.py
+++ Code_ForModified/Task2/wrong_5_043.py
@@ -1,4 +1,6 @@
 # id : wrong_5_043 
+# 错误原因：在计算时，获取到的x和y代表在长、宽上各应用到多少个a*a的正方形，因此在return结果时应该return(x*y)，而不应该对x和y进行相加操作
+# 修改方式：修改return的值即可
 def fun(input):  
     n,m,a = input.split() 
     n=int(n)
@@ -11,15 +13,20 @@ def fun(input):
             x = n//a
             if(m%a==0):
                 y=m//a
-                return(x+y-1)
+                return(x*y)
             else:
                 y=m//a + 1
-                return(x+y)
+                return(x*y)
         else:
             x= n//a +1
             if(m%a==0):
                 y=m//a
-                return(x+y)
+                return(x*y)
             else:
                 y= m//a +1
-                return(x+y)
+                return(x*y)
+
+# 获取输入数值时的代码段            
+imformation=input()
+print(fun(imformation))
+# wrong_5_043 end
