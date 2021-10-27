4c4b18105b80cf15f2be1601ad854a8f6c7acfca
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-27 19:24:28 +0800

    Update wrong_5_014.py

--- Code_ForModified/wrong_5_014.py
+++ Code_ForModified/wrong_5_014.py
@@ -1,5 +1,12 @@
+# Task 1:
+# 错误原因：2不能被划分为两偶数之和，因此需要特殊考虑Weight==2的情况。
 def fun(Weight):
-    if Weight % 2 == 0:
+    if (Weight % 2 == 0 and Weight != 2):
         return("YES")
     else:
         return("NO")
+
+# 获取输入数值时的代码段：
+w=int(input())
+print(fun(w))
+# Task 1 end
