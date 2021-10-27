88af94b85898fc866747377b021a2efe425c68f7
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-27 23:39:27 +0800

    Update wrong_5_010.py

--- Code_ForModified/Task2/wrong_5_010.py
+++ Code_ForModified/Task2/wrong_5_010.py
@@ -1,4 +1,6 @@
 # Task 2: wrong_5_010
+# 错误原因：思路整体出错
+# 修改方式：重新写一份代码，重写的python代码见Right_Task2.py文件中
 def fun(input):  
     n, m, a=map(int, input.split(" "))     
     surRect = n*m
@@ -7,3 +9,8 @@ def fun(input):
     if surRect%surFt != 0:
         needed += surFt / (surRect%surFt*needed)
     return(needed)
+
+# 获取输入数值时的代码段
+imformation=input()
+print(fun(imformation))
+# wrong_5_010 end            
