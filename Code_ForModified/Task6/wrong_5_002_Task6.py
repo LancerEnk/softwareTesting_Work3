9da63c803d4203e88d15ebb780856c7d2a6d402e
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 19:01:21 +0800

    Update wrong_5_002_Task6.py

--- Code_ForModified/Task6/wrong_5_002_Task6.py
+++ Code_ForModified/Task6/wrong_5_002_Task6.py
@@ -1,4 +1,6 @@
 # id : wrong_5_002
+# 错误原因：在现第10行进行遍历时，使用range(len(a)-1)，这实际给i取的是 0 <= i <= len(a)-2 的值，不能够使i遍历到a[i]和b[i]的最后一位。
+# 修改方案：将range(len(a)-1)改成range(len(a))
 def fun(input1,input2):
 	a = str(input1).lower()
 	b = str(input2).lower()
@@ -10,3 +12,9 @@ def fun(input1,input2):
 				return("-1")            
 			if (a[i] > b[i]):
 				return("1")
+
+# 获取输入数值时的代码段
+str1=input()
+str2=input()
+print(fun(str1,str2))
+# wrong_5_002 end
