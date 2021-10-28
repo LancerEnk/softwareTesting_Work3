0af7ba8dc5155b1b94edcb0e61857fb0dbfc4f2d
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 14:22:02 +0800

    Update wrong_5_047.py

--- Code_ForModified/Task3/wrong_5_047.py
+++ Code_ForModified/Task3/wrong_5_047.py
@@ -1,9 +1,17 @@
 # Task 3: wrong_5_047
+# 错误原因：if条件出错；i的范围是0~len(a)-1，这是数组的限制所导致的，因此在算人数时应该c=i+1
+# 修改方案：对if的条件进行修改，增加正数限制
 def fun(input1,input2):
 	n,k=map(int,input1.split())
 	a=[int(x) for x in input2.split()]
 	c=0
 	for i in range(0,len(a)):
-		if a[i]==k:
-			c=i-1 
+		if a[i]>=a[k-1] and a[i]>0 :
+			c=i+1 
 	return(c)
+
+# 获取输入数值时的代码段
+imformation1=input()
+imformation2=input()
+print(fun(imformation1,imformation2))
+# wrong_5_047 end 
