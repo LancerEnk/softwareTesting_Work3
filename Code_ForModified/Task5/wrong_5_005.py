2bcd1da5df1e088502457fabab00129b7addc969
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 16:51:46 +0800

    Update wrong_5_005.py

--- Code_ForModified/Task5/wrong_5_005.py
+++ Code_ForModified/Task5/wrong_5_005.py
@@ -1,4 +1,6 @@
 # Task 5: wrong_5_005
+# 错误原因：原第10行、现第12行的 s += '-' 出错，题干中的衔接是用'.'进行的，应该用 s += '.'
+# 修改方案：将 s += '-' 改为 s += '.'
 def fun(input):
 	k = str(input)
 	k = k.lower()
@@ -7,6 +9,11 @@ def fun(input):
 
 	for i in range(0, len(k)):
 		if k[i] not in L:
-			s += '-'
+			s += '.'
 			s += k[i]
 	return(s)
+
+# 获取输入数值时的代码段
+str1=input()
+print(fun(str1))
+# wrong_5_005 end
