c98ea8583230c1c6daac7144855e608456eeff38
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 14:13:23 +0800

    Update wrong_5_028.py

--- Code_ForModified/Task3/wrong_5_028.py
+++ Code_ForModified/Task3/wrong_5_028.py
@@ -1,9 +1,17 @@
 # Task 3: wrong_5_028
+# 错误原因：if条件错误
+# 修改方案：对if的条件进行修改，增加正数限制
 def fun(input1,input2):
 	n , k = map(int,input1.split())
 	a=list(map(int,input2.split()))
 	contestants=0
 	for i in a:
-		if i > k:
+		if i>=a[k-1] and i>0 :
 			contestants+=1
 	return(contestants)
+
+#获取输入数值时的代码段
+imformation1=input()
+imformation2=input()
+print(fun(imformation1,imformation2))
+# wrong_5_028 end
