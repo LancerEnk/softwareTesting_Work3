3c477f9aca48a867d13737c710a25e6a19d6d3c3
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 19:19:29 +0800

    Update wrong_5_048_Task6.py

--- Code_ForModified/Task6/wrong_5_048_Task6.py
+++ Code_ForModified/Task6/wrong_5_048_Task6.py
@@ -1,4 +1,11 @@
 # id : wrong_5_048
+# 错误原因：
+#	问题1：在所有的涉及到对ret进行改动的代码时，不应该给ret+=“1”、“-1”、“0”，而应该直接设置ret的字符值为“1”、“-1”、“0”。
+#	问题2：在str1和str2出现第一个不同的字母时，就已经完成结果的比较了，因此如果进入到input1[i].lower() != input2[i].lower()的分支中，
+#	      给ret赋完值以后应该直接return结果，不应该再接着比较下去。
+# 修改方案：
+#	问题1：将所有的ret+=“1”、“-1”、“0”改成ret=“1”、“-1”、“0”。
+#	问题2：在两个elif分支的末尾加上return ret。
 def fun(input1,input2):
 	ret ="";
 	n = len(input1)
@@ -6,8 +13,16 @@ def fun(input1,input2):
 		if input1[i].lower() == input2[i].lower():
 			pass
 		elif input1[i].lower() < input2[i].lower():
-			ret+=("-1")            
+			ret=("-1")
+			return ret
 		elif input1[i].lower() > input2[i].lower():
-			ret+=("1")            
-	ret+=("0")
+			ret=("1") 
+			return ret
+	ret=("0")
 	return ret
+
+# 获取输入数值时的代码段
+str1=input()
+str2=input()
+print(fun(str1,str2))
+# wrong_5_048 end
