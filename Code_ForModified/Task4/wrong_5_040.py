cf98b0117590ebb4dbe23a0a7838722854670d67
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 15:31:52 +0800

    Update wrong_5_040.py

--- Code_ForModified/Task4/wrong_5_040.py
+++ Code_ForModified/Task4/wrong_5_040.py
@@ -1,4 +1,12 @@
 # Task 4 : wrong_5_040
+# 错误原因：返回值return的计算方式错误
+# 修改方法：修改return中的表达式
+import math
 def fun(input):
     n,m = map(int,input.split())
-    return(int(n/m))
+    return((n*m)//2)
+
+# 获取输入数值时的代码段
+str1=input()
+print(fun(str1))
+# wrong_5_040 end
