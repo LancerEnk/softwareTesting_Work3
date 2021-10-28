967156f0628b789568b5eb5d8212ce270f04c8b0
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 16:08:02 +0800

    Update wrong_5_001.py

--- Code_ForModified/Task5/wrong_5_001.py
+++ Code_ForModified/Task5/wrong_5_001.py
@@ -1,11 +1,13 @@
 # Task 5: wrong_5_001
+# 错误原因：原第8行，现第10行的if条件出错，在判断时将a[i]=='U'的条件写成a[i]=='u'，后者重复两次，而且没有加入Y和y的判定。
+# 修改方案：修改现第10行的if条件。
 def fun(input):
 	a=input
 	b=[] 
 	c=(len(a)) 
 	ret = ""
 	for i in range(c):
-		if a[i] =='a' or  a[i] =='A' or a[i] =='e' or  a[i] =='E' or a[i] =='i' or  a[i] =='I' or a[i] =='o' or  a[i] =='O' or a[i] =='u' or  a[i] =='u':
+		if a[i] =='a' or  a[i] =='A' or a[i] =='e' or  a[i] =='E' or a[i] =='i' or  a[i] =='I' or a[i] =='o' or  a[i] =='O' or a[i] =='u' or  a[i] =='U' or a[i] =='y' or a[i] =='Y':
 			continue
 		else:
 			k=ord(a[i]) 
@@ -20,3 +22,7 @@ def fun(input):
 		#print(b[i],end="")
 		ret += b[i]
 	return ret
+
+# 获取输入数值时的代码段
+str1=input()
+print(fun(str1))
