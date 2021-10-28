0e4dd3546ebdae4e3ff8b4c401b248295ba000a1
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 22:26:31 +0800

    Update wrong_5_041.py

--- Code_ForModified/Task8/wrong_5_041.py
+++ Code_ForModified/Task8/wrong_5_041.py
@@ -1,4 +1,6 @@
 # id : wrong_5_041
+# 错误原因：在第11行有return n，这意味着程序进行到此步将直接结束运行，不会再进行下面第12行~20行的步骤，即数字之间不会再＋“+”。
+# 修改方案：注释/删除这行return n。
 def fun(input):
 	a=input
 	l=len(a)
@@ -8,7 +10,7 @@ def fun(input):
 		n.append(int(a[i]))    
 		i=i+2    
 	n.sort()
-	return n
+	# return n
 	m=""
 	for x in range(0,len(n)):
 		s=str(n[x])
@@ -18,3 +20,9 @@ def fun(input):
 			s=str(n[x])  
 			m=m+s+"+"        
 	return m
+
+# 获取输入数值时的代码段
+# 输入格式为："1+2+1+2+2+2+2+1+3+3"
+str1=eval(input())
+print(fun(str1))
+# wrong_5_017 end
