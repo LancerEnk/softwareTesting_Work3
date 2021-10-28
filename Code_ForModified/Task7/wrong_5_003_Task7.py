09f49044fe3ca6deb5dee32f220d21093997a452
LancerEnk <88569335+LancerEnk@users.noreply.github.com>
2021-10-28 21:44:58 +0800

    Update wrong_5_003_Task7.py

--- Code_ForModified/Task7/wrong_5_003_Task7.py
+++ Code_ForModified/Task7/wrong_5_003_Task7.py
@@ -1,4 +1,6 @@
 # id : wrong_5_003
+# 错误原因：在最终return的返回值计算过程中，应该给(x-2)和(y-2)分别取绝对值，不应该取abs((x-2)+(y-2))。
+# 修改方案：修改return (abs(x+y-4))为return(abs(x-2)+abs(y-2))。
 def fun(input):
 	ar=[]
 	x=0
@@ -13,4 +15,10 @@ def fun(input):
 				x=i
 				y=j
 				break
-	return(abs(x+y-4))
+	return(abs(x-2)+abs(y-2))
+
+# 获取输入数值时的代码段
+# 输入格式为：["0 0 0 0 0","0 0 0 0 0","0 1 0 0 0","0 0 0 0 0","0 0 0 0 0"]
+str1=eval(input())
+print(fun(str1))
+# wrong_5_003 end
